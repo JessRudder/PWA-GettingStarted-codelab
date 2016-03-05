a826ee9f38f99cda89ea385142f878f022932911
JessRudder <thejessicarudder@gmail.com>
2016-03-05 14:04:16 -0500

    Add check for browser ability to handle progressive web app functionality

--- scripts/app.js
+++ scripts/app.js
@@ -207,4 +207,10 @@
     app.saveSelectedCities();
   }
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+
 })();
